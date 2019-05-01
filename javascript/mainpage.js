0e7812b4df10d4739b754f28b4ac0e47af52962c
Binay <binaydhawa@gmail.com>
2019-05-01 21:47:40 +1000

    Modal Added into the page

--- javascript/mainpage.js
+++ javascript/mainpage.js
@@ -22,7 +22,44 @@ function sidenav()
         document.getElementById("sidebar").style.width="300px";
         document.getElementById("homepage").style.display="block";
         sidenav_button= true;    
+    }  
+}
+
+function homepage()
+{
+    if (sidenav_button==true)
+    {
+        document.getElementById("homepage").style.display="block";
+        document.getElementById("data").style.display="none";
     }
+}
+
+function data()
+{
+    if (sidenav_button==true)
+    {
+        document.getElementById("homepage").style.display="none";
+        document.getElementById("data").style.display="block";
+    }
+}
+
+function About()
+{
+    //alert("sdfsd");
+    document.getElementById("About").style.display="block";
+    //document.getElementsByClassName("")
+}
+
+function Help()
+{
+    document.getElementById("Help").style.display="block";
+}
+
+function close_modal()
+{  
+    document.getElementById("About").style.display="none";
+    document.getElementById("Help").style.display="none";
+
     
 }
 
