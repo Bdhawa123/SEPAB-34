89106680967b539148d1eec97a0e6bec0552a004
quan <cyq3311@gmail.com>
2019-06-19 15:52:55 +1000

    added sample drawing function usage

--- javascript/mainpage.js
+++ javascript/mainpage.js
@@ -1,12 +1,62 @@
-var map; // map initializer
-var sidenav_button = false;
-    function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -37.819344, lng: 145.040506 },
-        zoom: 16
-      });
-    }
+//Google Maps Init, centered at swinburne hawthorn
+var map; 
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -37.819344, lng: 145.040506 },
+    zoom: 16
+  });
+}
 
+//Add marker function Google Maps
+function addMarker(myLatLng, name){
+    marker = new google.maps.Marker({
+        position: myLatLng,
+        map:map,
+        title:name
+    });
+}
+
+//Draw path function Google Maps
+function drawPolyLine(wheelchairPath){
+    path = new google.maps.Polyline({
+        path: wheelchairPath,
+        geodesic: true,
+        strokeColor: "#000000",
+        strokeOpacity: 1.0,
+        strokeWeight: 10
+    })
+    path.setMap(map);
+}
+
+window.onload = function(){
+    var csvData;
+    $.get("dataprototype/GPSData.csv", function(data){
+        csvData = $.csv.toObjects(data);
+        console.log(csvData);
+
+        //50 Markers from data
+        for(i=0; i<50; i++){
+            var point = csvData[i];
+            latlng = new google.maps.LatLng(point.latitude, point.longitude);
+            console.log(latlng)
+            name = point.Name;
+            addMarker(latlng, name);
+        }
+
+        //Polylines from 50 data points
+        var wheelchairPath = [];
+        for(i=0; i<50; i++){
+            var point = csvData[i];
+            latlng = new google.maps.LatLng(point.latitude, point.longitude);
+            wheelchairPath.push(latlng);
+        }
+        drawPolyLine(wheelchairPath)
+    })
+
+}
+
+//navigation functions
+var sidenav_button = false;
 function sidenav()
 {
     if (sidenav_button==true)
