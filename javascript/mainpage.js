e7c2d7047ac631a4edeafaa360ae81991dd3a4b8
quan <cyq3311@gmail.com>
2019-06-19 13:54:08 +1000

    code cleaning

--- javascript/mainpage.js
+++ javascript/mainpage.js
@@ -2,17 +2,15 @@ var map; // map initializer
 var sidenav_button = false;
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
+        center: { lat: -37.819344, lng: 145.040506 },
+        zoom: 16
       });
     }
-
 
 function sidenav()
 {
     if (sidenav_button==true)
     {
-       
         document.getElementById("sidebar").style.width="0";
         document.getElementById("homepage").style.display="none";
         //should change css of the side nav, display 0 and add transition
