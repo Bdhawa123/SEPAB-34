8c7037d39a17366bc4e42ca8a2ca860c7c3eedd7
quan <cyq3311@gmail.com>
2019-07-10 14:10:18 +1000

    Speed Color

--- javascript/mainpage.js
+++ javascript/mainpage.js
@@ -1,5 +1,29 @@
+// function setMapOnAll(map) {
+//     for (var i = 0; i < markers.length; i++) {
+//         markers[i].setMap(map);
+//     }
+// }
+
+// // Removes the markers from the map, but keeps them in the array.
+// function clearMarkers() {
+//     setMapOnAll(null);
+// }
+
+// // Shows any markers currently in the array.
+// function showMarkers() {
+//     setMapOnAll(map);
+// }
+
+// // Deletes all markers in the array by removing references to them.
+// function deleteMarkers() {
+//     clearMarkers();
+//     markers = [];
+// }
+
+
 //Google Maps Init, centered at swinburne hawthorn
 var map; 
+var markers = [];
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -37.819344, lng: 145.040506 },
@@ -14,6 +38,7 @@ function addMarker(myLatLng, name){
         map:map,
         title:name
     });
+    markers.push(marker);
 }
 
 //Draw path function Google Maps
@@ -28,29 +53,145 @@ function drawPolyLine(wheelchairPath){
     path.setMap(map);
 }
 
+function drawPolyLineSection(datapoint){
+    path = new google.maps.Polyline({
+        path: [datapoint.latitude, datapoint.longitude],
+        geodesic: true,
+        strokeColor: "#000000",
+        strokeOpacity: 1.0,
+        strokeWeight: 10
+    })
+    path.setMap(map);
+}
+
+function removePath(){
+    path.setMap(null);
+}
+
+function drawPath(start, end, csvData){
+    var startpoint = parseInt(start);
+    var endpoint = parseInt(end);
+    var wheelchairPath = [];
+    for(i=startpoint; i<endpoint; i++){
+        // console.log(startpoint, endpoint)
+        var point = csvData[i];
+        latlng = new google.maps.LatLng(point.latitude, point.longitude);
+        wheelchairPath.push(latlng);
+    }
+    drawPolyLine(wheelchairPath);
+}
+
+function drawMarkers(start, end, csvData){
+    for(i=parseInt(start); i<parseInt(end); i++){
+        var point = csvData[i];
+        latlng = new google.maps.LatLng(point.latitude, point.longitude);
+        // console.log(latlng)
+        name = point.Name;
+        addMarker(latlng, name);
+    }
+}
+
 window.onload = function(){
     var csvData;
     $.get("dataprototype/GPSData.csv", function(data){
         csvData = $.csv.toObjects(data);
         console.log(csvData);
+        var min = 0;
+        var max = 50;
 
         //50 Markers from data
-        for(i=0; i<50; i++){
-            var point = csvData[i];
-            latlng = new google.maps.LatLng(point.latitude, point.longitude);
-            console.log(latlng)
-            name = point.Name;
-            addMarker(latlng, name);
+        // drawMarkers(min, max, csvData);
+        // console.log(markers)
+
+        //Polylines from first 50 data points
+        // drawPath(min, max, csvData)
+
+        var Colors = [
+            "#FF0000", 
+            "#00FF00", 
+            "#0000FF", 
+            
+        ];
+
+        function color(speed){
+            if(speed<10)
+                return "#FF0000";
+            else if(speed<40)
+                return "#00FF00";
+            else if(speed<70)
+                return "#0000FF";
         }
 
-        //Polylines from 50 data points
-        var wheelchairPath = [];
-        for(i=0; i<50; i++){
-            var point = csvData[i];
-            latlng = new google.maps.LatLng(point.latitude, point.longitude);
-            wheelchairPath.push(latlng);
+        function distance(lat1, lon1, lat2, lon2, unit) {
+            if ((lat1 == lat2) && (lon1 == lon2)) {
+                return 0;
+            }
+            else {
+                var radlat1 = Math.PI * lat1/180;
+                var radlat2 = Math.PI * lat2/180;
+                var theta = lon1-lon2;
+                var radtheta = Math.PI * theta/180;
+                var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+                if (dist > 1) {
+                    dist = 1;
+                }
+                dist = Math.acos(dist);
+                dist = dist * 180/Math.PI;
+                dist = dist * 60 * 1.1515;
+                if (unit=="K") { dist = dist * 1.609344 }
+                if (unit=="N") { dist = dist * 0.8684 }
+                return dist;
+            }
         }
-        drawPolyLine(wheelchairPath)
+
+        for(i=min;i<max;i++){
+            var point = new google.maps.LatLng(csvData[i].latitude, csvData[i].longitude);
+            var point2 = new google.maps.LatLng(csvData[i+1].latitude, csvData[i+1].longitude);
+
+            var speed = distance(point.lat(), point.lng(), point2.lat(), point.lng(), "K")/(1/600);
+            
+            path = new google.maps.Polyline({
+                path: [point, point2],
+                geodesic: true,
+                strokeColor: color(speed),
+                strokeOpacity: 1.0,
+                strokeWeight: 10,
+                map:map
+            })
+        }
+   
+        //slider function
+        $("#slider-range").slider({
+            range: true,
+            min: min,
+            max: max,
+            values: [min, max],
+            slide: function( event, ui ) {
+                $("#datapoint").val( "Point: " + ui.values[0] + " - Point: " + ui.values[1] );
+                
+                //remove paths
+                removePath()
+
+                //draw polylines
+                drawPath(ui.values[0], ui.values[1], csvData);
+
+                // //start: hide previous marker and show current marker
+                // if(ui.values[0] != min){
+                //     markers[ui.values[0] - 1].setMap(null);
+                // }
+                // markers[ui.values[0]].setMap(map);
+
+                // //end: hide previous marker and show current marker
+                // if(ui.values[1] != max){
+                //     markers[ui.values[1]].setMap(null);
+                // }
+                // markers[ui.values[1]-1].setMap(map);
+            }
+        });
+
+        //Default values
+        $("#datapoint").val("Point: " + $("#slider-range").slider("values", 0) + " - Point: " + $("#slider-range").slider("values", 1));
+      
     })
 
 }
