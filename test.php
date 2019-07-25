49bdaa3f351403934cacad5568a4db9a7691fa13
Binay <binaydhawa@gmail.com>
2019-07-26 00:04:08 +1000

    Upload working with validation for file type

--- test.php
+++ test.php
@@ -6,19 +6,44 @@
 
     fclose($myfile);
     */
-    echo sizeof($_FILES);
+
+    //validate file type
+    function validate($filetype)
+    {
+        if (trim(strtolower($filetype))=="csv")
+        {   
+            return true;
+        }
+        else
+        {           
+            return false;
+        }
+    
+    }
+
 
     for($val =0; $val<sizeof($_FILES); $val++)
     {
-        echo $_FILES['file'.$val]['name']."\r\n";
-   
+        //get file extension
+        $filename = $_FILES['file'.$val]['name']."\r\n";        
+        $filetype = explode(".",$filename);    
+        $filetype = $filetype[sizeof($filetype)-1];
 
+        $valid = validate($filetype);     
+       if ($valid==false)
+       {
+            http_response_code(400);                                                        //set response code to false
+       }
     }
     
+    
+
+    //database execution
+
 
     //place where the files get temperorily saved
-    $file2upload = $_FILES['file1']['tmp_name']; // $_FILES['id']['array']-> name, type, error, size
-    echo $file2upload; 
+    //$file2upload = $_FILES['file0']['tmp_name']; // $_FILES['id']['array']-> name, type, error, size
+    //echo $file2upload; 
    /* if(move_uploaded_file($file2upload,'test/'.basename($_FILES["file"]["name"])))//upload file(file,directory and the filename(filename needs to be there otherwise it won't work))
     {
         
