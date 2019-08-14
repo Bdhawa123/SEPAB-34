4762e90812fd6a1472fc037811b1caa1f8a5cc9e
quan <cyq3311@gmail.com>
2019-08-15 01:37:31 +1000

    Add dynamic alternative button for each circle

--- javascript/leaflet_map.js
+++ javascript/leaflet_map.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-mixed-operators */
+const { L, d3 } = window; // Define L, d3
+
 let csvData;
 let min;
 let max;
@@ -49,6 +51,11 @@ function sliderInit() {
   document.getElementById('datapoint').value = str;
 }
 
+// d3 color function
+const color = d3.scaleQuantize()
+  .domain([0, 100])
+  .range(['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee090', '#ffffbf', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#313695']);
+
 // function to calculate distance between 2 points
 function distance(lat1, lon1, lat2, lon2, unit) {
   if ((lat1 === lat2) && (lon1 === lon2)) {
@@ -79,9 +86,37 @@ function distance(lat1, lon1, lat2, lon2, unit) {
   return dist;
 }
 
+function drawPolylines() {
+  for (let i = min; i < max; i += 1) {
+    const lat1 = parseFloat(csvData[i].latitude);
+    const lon1 = parseFloat(csvData[i].longitude);
+    const lat2 = parseFloat(csvData[i + 1].latitude);
+    const lon2 = parseFloat(csvData[i + 1].longitude);
+
+    const latlngs = [L.latLng(lat1, lon1), L.latLng(lat2, lon2)];
+    const speed = distance(
+      lat1, lon1,
+      lat2, lon2,
+      'K',
+    ) / (1 / 600);
+
+    const polyline = L.polyline(latlngs, {
+      color: color(speed),
+      weight: 8,
+      lineCap: 'square',
+      smoothFactor: 1,
+    }).addTo(map);
+
+    const polylineTooltipText = `${String(speed.toFixed(2))} km/h`;
+    polyline.bindTooltip(polylineTooltipText).closeTooltip();
+
+    polylines.push(polyline);
+  }
+}
+
 // Dom content loaded
 document.addEventListener('DOMContentLoaded', () => {
-  const { L, d3 } = window; // Define L, d3
+  // const { L, d3 } = window; // Define L, d3
 
   map = L.map('mapid', {
     zoomControl: false,
@@ -98,24 +133,22 @@ document.addEventListener('DOMContentLoaded', () => {
     position: 'topright',
   }).addTo(map);
 
-  // d3 color function
-  const color = d3.scaleQuantize()
-    .domain([0, 100])
-    .range(['#a50026', '#d73027', '#f46d43', '#fdae61', '#fee090', '#ffffbf', '#e0f3f8', '#abd9e9', '#74add1', '#4575b4', '#313695']);
-
   // load data
   $.get('dataprototype/GPSData.csv', (data) => {
     csvData = $.csv.toObjects(data);
     min = 0;
     max = 50;
 
+    // add circles
     const circle = L.circle([parseFloat(csvData[0].latitude), parseFloat(csvData[0].longitude)], {
       radius: 800,
     }).addTo(map);
 
+    // circle add tooltip
     const circleTooltipText = 'Swinburne';
     circle.bindTooltip(circleTooltipText).openTooltip();
 
+    // circle click event listener
     circle.on('click', () => {
       // hide circle
       map.removeLayer(circle);
@@ -124,37 +157,39 @@ document.addEventListener('DOMContentLoaded', () => {
       const latlngCircle = circle.getLatLng();
       map.flyTo(latlngCircle, 16);
 
-      // code is fired after animation ends
+      // code is fired after animation ends, draw paths and slider
       map.once('moveend', () => {
-        for (let i = min; i < max; i += 1) {
-          const lat1 = parseFloat(csvData[i].latitude);
-          const lon1 = parseFloat(csvData[i].longitude);
-          const lat2 = parseFloat(csvData[i + 1].latitude);
-          const lon2 = parseFloat(csvData[i + 1].longitude);
-
-          const latlngs = [L.latLng(lat1, lon1), L.latLng(lat2, lon2)];
-          const speed = distance(
-            lat1, lon1,
-            lat2, lon2,
-            'K',
-          ) / (1 / 600);
-
-          const polyline = L.polyline(latlngs, {
-            color: color(speed),
-            weight: 8,
-            lineCap: 'square',
-            smoothFactor: 1,
-          }).addTo(map);
-
-          const polylineTooltipText = `${String(speed.toFixed(2))} km/h`;
-          polyline.bindTooltip(polylineTooltipText).closeTooltip();
-
-          polylines.push(polyline);
-
-          // init slider
-          sliderInit();
-        }
+        // draw polylines
+        drawPolylines();
+        // init slider
+        sliderInit();
       });
     });
+
+    // create a dynamic alternative button for each circle
+    const button = document.createElement('button');
+    button.innerHTML = circleTooltipText;
+    button.id = circleTooltipText;
+    button.classList.add('btn');
+    button.classList.add('btn-primary');
+
+    // button click event listener
+    button.addEventListener('click', () => {
+      // hide circle
+      map.removeLayer(circle);
+
+      // fly to circle's latlng
+      const latlngCircle = circle.getLatLng();
+      map.flyTo(latlngCircle, 16);
+
+      // code is fired after animation ends, draw paths and slider
+      map.once('moveend', () => {
+        // draw polylines
+        drawPolylines();
+        // init slider
+        sliderInit();
+      });
+    });
+    document.querySelector('.data-buttons').appendChild(button);
   });
 });
