942816b352517ecff84c5282009c430f5b35b483
quan <cyq3311@gmail.com>
2019-09-05 13:32:23 +1000

    Resolve parity

--- prototype.php
+++ prototype.php
@@ -18,15 +18,8 @@ if (isset($_SESSION['id'])) {
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <!-- TODO create local fallback -->
-
-    <!-- D3 JS library -->
-    <script src="https://d3js.org/d3.v4.min.js"></script>
-
-    <!-- Main JS file -->
-    <script type="text/javascript" src="javascript/mainpage.js"></script>
-    <script type="text/javascript" src="javascript/mainpage-ui.js"></script>
-  
+    <!-- Leaflet CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
 
     <!-- CSS file -->
     <link rel="stylesheet" href="css/main.css" />
@@ -60,13 +53,10 @@ if (isset($_SESSION['id'])) {
 
     <!-- Map -->
     <div class="container-fluid overflow-hidden">
-
-
-
-      <div id="map"></div>
+      <!-- <div id="map"></div> -->
+      <!-- Leaflet Maps -->
+      <div id="mapid"></div>
       <div id="slider-range"></div>
-
-
 
       <div id="mySidenav" class="sidenav">
         <div class="demo-card-wide mdl-card mdl-shadow--2dp">
@@ -180,88 +170,87 @@ if (isset($_SESSION['id'])) {
         </p>
       </div>
 
-  </div>
+    </div>
 
-  </div>
+    </div>
 
 
-      <!-- About modal -->
-      <div class="modal fade" id="about_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">About</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              About goes here
-            </div>
+    <!-- About modal -->
+    <div class="modal fade" id="about_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">About</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            About goes here
           </div>
         </div>
       </div>
+    </div>
 
-      <!-- Help modal -->
-      <div class="modal fade" id="help_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Help</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              Documentation of how it would work
-            </div>
+    <!-- Help modal -->
+    <div class="modal fade" id="help_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Help</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            Documentation of how it would work
           </div>
         </div>
       </div>
+    </div>
 
-      <!-- Login modal -->
-      <div class="modal fade" id="login_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Login</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              <form action="prototype.php" method="post">
-                <div class="form-group">
-                  <label for="username" class="col-form-label">Username:</label>
-                  <input type="text" name="username" class="form-control" id="username" />
-                </div>
-                <div class="form-group">
-                  <label for="password" class="col-form-label">Password:</label>
-                  <input type="password" name="password" class="form-control" id="password" />
-                </div>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <input type="submit" name="login" class="btn btn-primary" value="Login" />
-            </div>
-            </form>
+    <!-- Login modal -->
+    <div class="modal fade" id="login_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
           </div>
+          <div class="modal-body">
+            <form action="prototype.php" method="post">
+              <div class="form-group">
+                <label for="username" class="col-form-label">Username:</label>
+                <input type="text" name="username" class="form-control" id="username" />
+              </div>
+              <div class="form-group">
+                <label for="password" class="col-form-label">Password:</label>
+                <input type="password" name="password" class="form-control" id="password" />
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <input type="submit" name="login" class="btn btn-primary" value="Login" />
+          </div>
+          </form>
         </div>
       </div>
+    </div>
 
-      <!-- Import modal -->
-      <div class="modal fade" id="import_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Import Data</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body container align-items-center">
-          <!--<form onsubmit="submit_file(event)">
+    <!-- Import modal -->
+    <div class="modal fade" id="import_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Import Data</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body container align-items-center">
+            <!--<form onsubmit="submit_file(event)">
             <div id ="drag" align="center">Drag and Drop .csv here <br>
               <input type="file" multiple name="button_input">
             </div> 
@@ -273,61 +262,76 @@ if (isset($_SESSION['id'])) {
             <input type="submit" align="center" class="btn btn-primary" value="Submit">
           </form>-->
             <div>
-              <form onsubmit="submit_file(event)"> 
-                <input type="file" class="hd_inp" id="fileA">           <!--File A should be the first gps co-ordinates-->
-                <input type="file" class="hd_inp" id ="fileB">          <!--File B should be the second data set of gyro and other variations-->
-                
-                
-                <div id ="dropzone" class="row container d-flex justify-content-center">                                    <!--Drop Zone to Each click will trigger file A or B respectively-->      
-                      <div id="fileone" class="innercontainer col-sm-5 align-content-end" ondrop ="dropHandler(event,'fileimg1')" ondragover="dragOverHandler(event)">        <!--drop listener for fileone-->                            
-                            <div id="fileimg1" class="fileimg row"> 
-                            </div> 
-                            <div id="fileimg12" class="fileimg"> 
-                                File A
-                          </div>                  
-                      </div>
-                      <div class="col-sm-2">
-                        
-                      </div>
-                
+              <form onsubmit="submit_file(event)">
+                <input type="file" class="hd_inp" id="fileA">
+                <!--File A should be the first gps co-ordinates-->
+                <input type="file" class="hd_inp" id="fileB">
+                <!--File B should be the second data set of gyro and other variations-->
 
-                      <div id ="filetwo" class="innercontainer col-sm-5  align-content-end" ondrop ="dropHandler(event,'fileimg2')" ondragover="dragOverHandler(event)">      <!--drop listener for filetwo-->
-                         
-                          <div id="fileimg2" class="fileimg">
-                              </div>
-                          <div id="fileimg22" class="fileimg"> 
-                              File B
-                          </div> 
-                      </div>
-                  
+
+                <div id="dropzone" class="row container d-flex justify-content-center">
+                  <!--Drop Zone to Each click will trigger file A or B respectively-->
+                  <div id="fileone" class="innercontainer col-sm-5 align-content-end" ondrop="dropHandler(event,'fileimg1')" ondragover="dragOverHandler(event)">
+                    <!--drop listener for fileone-->
+                    <div id="fileimg1" class="fileimg row">
+                    </div>
+                    <div id="fileimg12" class="fileimg">
+                      File A
+                    </div>
+                  </div>
+                  <div class="col-sm-2">
+
+                  </div>
+
+
+                  <div id="filetwo" class="innercontainer col-sm-5  align-content-end" ondrop="dropHandler(event,'fileimg2')" ondragover="dragOverHandler(event)">
+                    <!--drop listener for filetwo-->
+
+                    <div id="fileimg2" class="fileimg">
+                    </div>
+                    <div id="fileimg22" class="fileimg">
+                      File B
+                    </div>
+                  </div>
+
                 </div>
 
                 <input type="submit" align="center" class="btn btn-primary align-items-center" value="Submit">
 
-              </form> 
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-        <!-- scripts-->
-        <!-- jQuery CDN -->
-        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <!-- Popper JS CDN -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <!-- Bootstrap JS CDN -->
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <!-- scripts-->
+      <!-- jQuery CDN -->
+      <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+      <!-- Popper JS CDN -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <!-- Bootstrap JS CDN -->
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-        <!-- jQuery CSV JS-->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/1.0.3/jquery.csv.min.js"></script>
+      <!-- jQuery CSV JS-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/1.0.3/jquery.csv.min.js"></script>
 
-        <!-- Google Maps API -->
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZxhwbU7de4SpOdGBu3KnTNxJqUyQHMxI&callback=initMap" async defer></script>
+      <!-- Google Maps API -->
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZxhwbU7de4SpOdGBu3KnTNxJqUyQHMxI&callback=initMap" async defer></script>
 
-        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 
-        <!-- Custom UI CSS -->
-        <script type="text/javascript" src="javascript/mainpage-ui.js"></script>
+      <!-- Leaflet -->
+      <script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js" integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og==" crossorigin=""></script>
+
+      <!-- D3 JS library -->
+      <script src="https://d3js.org/d3.v4.min.js"></script>
+
+      <!-- Main JS file -->
+      <script type="text/javascript" src="javascript/mainpage.js"></script>
+      <script type="text/javascript" src="javascript/mainpage-ui.js"></script>
+
+      <!-- Custom UI CSS -->
+      <script type="text/javascript" src="javascript/mainpage-ui.js"></script>
 
   </body>
 
